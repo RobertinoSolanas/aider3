a2c97a7e4c741dbf79ed9788f322a054291b4921
Robertino Solanas <r.sol@gmx.de>
2025-03-17 18:29:07 +0100

    spring-boot

---
+++ src/main/java/com/example/aider3/Aider3Application.java
@@ -0,0 +1,13 @@
+package com.example.aider3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aider3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aider3Application.class, args);
+	}
+
+}
