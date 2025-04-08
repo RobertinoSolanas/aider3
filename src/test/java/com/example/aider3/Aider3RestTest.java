10abe6f0e1aa328d66a57e9615587f93eedd729b
Robertino Solanas <r.sol@gmx.de>
2025-04-08 19:13:04 +0200

    corrections

--- src/test/java/com/example/aider3/Aider3RestTest.java
+++ src/test/java/com/example/aider3/Aider3RestTest.java
@@ -1,12 +1,10 @@
-package com.example;
+package com.example.aider3;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-
-import com.example.Aider3Rest;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.hamcrest.Matchers.notNullValue;
